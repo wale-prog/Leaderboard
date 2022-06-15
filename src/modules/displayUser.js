404977577721d627a55b590e89c089c27b0ab008
Olawale <olapetanwale@outlook.com>
2022-06-15 23:01:08 +0100

    Implement required changes

--- src/modules/displayUser.js
+++ src/modules/displayUser.js
@@ -2,23 +2,22 @@ import generate from './generateUsers.js';
 
 const gameId = 'pRs2LPxCUOU3ynZslV7H';
 
-async function fetchData() {
+const fetchData = async () => {
   const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
   const users = await response.json();
   return users;
-}
+};
 
-export async function singleRun() {
+export const singleRun = async () => {
   let myData = [];
   myData = await fetchData();
   const users = myData.result;
-
   users.forEach((item) => {
     generate(item);
   });
-}
+};
 
-export function getUsers() {
+export const getUsers = () => {
   const refreshBtn = document.querySelector('.refresh');
   const listItems = document.querySelector('.list-items');
   refreshBtn.addEventListener('click', async (e) => {
@@ -26,4 +25,4 @@ export function getUsers() {
     listItems.innerHTML = '';
     singleRun();
   });
-}
+};
